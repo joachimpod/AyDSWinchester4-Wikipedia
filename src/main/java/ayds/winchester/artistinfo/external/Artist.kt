c571de663eec1dc29cdbbf0ed3fa607c491631ca
joachimpod <joachimpodlesch@hotmail.com>
2023-05-29 16:43:10 -0300

    logo

--- src/main/java/ayds/winchester/artistinfo/external/Artist.kt
+++ src/main/java/ayds/winchester/artistinfo/external/Artist.kt
@@ -1,6 +1,7 @@
 package ayds.winchester.artistinfo.external
 
 const val BASE_WIKI_URL = "https://en.wikipedia.org/?curid="
+const val DEFAULT_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/8/8c/Wikipedia-logo-v2-es.png"
 
 data class WikipediaArtistInfo (
     val artistName: String,
